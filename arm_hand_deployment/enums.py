7f5b411fedffe5bdc0d067f324c5cfef60d117d7
Sicheng He <sichengh@usc.edu>
2025-01-13 14:32:56 -0800

    some refactoring

---
+++ arm_hand_deployment/enums.py
@@ -0,0 +1,5 @@
+import enum
+
+
+class ArmHandType(enum.Enum):
+    FRANKA = "franka"  # franka arm + franka gripper
--- run_server.py
+++ run_server.py
@@ -5,43 +5,23 @@ import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-from loguru import logger
-
-from concurrent import futures
-
-import grpc
-
-from arm_hand_deployment.franka.proto import service_pb2_grpc
-
 from typing import Optional
 
-from arm_hand_deployment.franka.communication.server import RobotService
+from arm_hand_deployment.enums import ArmHandType
 
 
 @hydra.main(config_path=CONFIG_PATH, config_name="config", version_base="1.3")
 def main(cfg: DictConfig):
-    print(OmegaConf.to_yaml(cfg))
-    port: int = int(cfg.rpc_port)
 
-    fci_ip: str = cfg.fci_ip
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    arm_hand_type = ArmHandType(cfg.arm_hand_type)
+    print(arm_hand_type)
 
-    # TODO: specify deoxys config files in config.yaml
-    service_pb2_grpc.add_FrankaServiceServicer_to_server(
-        RobotService(fci_ip,
-                     deoxys_general_cfg_file=os.path.join(
-                         CONFIG_PATH, "deoxys_config.yaml"),
-                     deoxys_osc_controller_cfg_file=os.path.join(
-                         CONFIG_PATH, "osc-pose-controller.yaml"),
-                     deoxys_joint_position_controller_cfg_file=os.path.join(CONFIG_PATH,
-                                                                            "joint-position-controller.yaml"),
-                     deoxys_joint_impedance_controller_cfg_file=os.path.join(CONFIG_PATH,
-                                                                             "joint-impedance-controller.yaml"),
-                     ), server)
-    server.add_insecure_port(f"[::]:{port}")
-    server.start()
-    logger.info(f"Server started, listening on {port}")
-    server.wait_for_termination()
+    if arm_hand_type == ArmHandType.FRANKA:
+        from arm_hand_deployment.franka import run_server
+    else:
+        raise NotImplementedError(
+            f"ArmHandType {arm_hand_type} not implemented")
+    run_server(cfg)
 
 
 if __name__ == '__main__':
